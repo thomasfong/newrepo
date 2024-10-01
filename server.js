f35c42a06620ddb5220e5e5f09910dc95f349348
thomas <thomasfong102@gmail.com>
2024-10-02 05:43:20 +0800

    Change Index route in server.js files

--- server.js
+++ server.js
@@ -23,11 +23,6 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-//Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
-
 // Index route
 app.get("/", baseController.buildHome)
 

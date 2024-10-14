898d3e71f0d6f8b4d7901ba77c8b7f965a40eba9
thomas <thomasfong102@gmail.com>
2024-10-15 05:24:23 +0800

    update

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -1,8 +1,6 @@
 CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
-ALTER TYPE public.account_type
-    OWNER TO cse340;
 
 -- Table structure for table `classification`
 CREATE TABLE public .classification (
--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,3 +1,5 @@
+const pool = require("../database/")
+
 async function getClassifications() {
   return await pool.query(
     "SELECT * FROM public.classification ORDER BY classification_name"

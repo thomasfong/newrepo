8655a1b7f31d99cb961f1791c0396365965a5ce4
thomas <thomasfong102@gmail.com>
2024-10-19 18:51:59 +0800

    update database

--- server.js
+++ server.js
@@ -12,6 +12,30 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities")
+const session = require("express-session")
+const pool = require('./database/')
+const cookieParser = require("cookie-parser")
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and Templates
@@ -19,13 +43,15 @@ const inventoryRoute = require("./routes/inventoryRoute")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+app.use(cookieParser())
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(require("./routes/static"))
 // Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", require("./routes/inventoryRoute"))
 
@@ -34,6 +60,9 @@ app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
 
+// Error maker route
+app.use('/inv', utilities.handleErrors(errorController.makeError))
+
 /* ***********************
 * Express Error Handler
 * Place after all other middleware
@@ -41,9 +70,11 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
+    image: "/images/upgrades/flux-cap.png",
     nav
   })
 })

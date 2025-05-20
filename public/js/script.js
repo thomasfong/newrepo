3334f4ba1b62eadcd34cc205b5a6a0e1af32d17b
ckangel <ckajeigbe1@gmail.com>
2025-05-20 08:21:59 +0000

    commit site creation

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,8 @@
-// Example of importing modules into index.js
-import { loadNavigation } from './nav.js';
-import { renderReviews } from './reviews.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  loadNavigation();
-  renderReviews();
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+    document.querySelector(".nav-bar").classList.toggle("active");
 });
+
+document.querySelector("button").addEventListener("click", function () {
+    alert("Congratulations! You've selected the DMC Delorean.");
+});
+

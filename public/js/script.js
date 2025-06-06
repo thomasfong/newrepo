8dd261eddb39057d58016657938430c3c3da5355
ckangel <ckajeigbe1@gmail.com>
2025-06-06 08:49:57 +0000

    Responsiveness of site

--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,19 @@
 //     alert("Congratulations! You've selected the DMC Delorean.");
 // });
 
+
+// TOGGLE BUTTON 
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('toggleButton');
+    const navMenu = document.getElementById('navMenu');
+
+    toggleButton.addEventListener('click', function () {
+      navMenu.classList.toggle('show');
+      this.innerHTML = navMenu.classList.contains('show')
+        ? '<i class="fa fa-x"></i>'
+        : '<i class="fa fa-bars"></i>';
+    });
+  });
    // Password toggle functionality
    const togglePassword = document.getElementById('togglePassword');
    const passwordInput = document.getElementById('account_password');
